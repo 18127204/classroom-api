7174c1984f94bd6cb5d84d7992e9e14f2e044487
18127024 <quangminhtran323@gmail.com>
2021-11-03 17:05:03 +0700

    first commit

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const usersRouter = require('./routes/users');
 const classesRouter=require('./api/classes')
 
 const app = express();
-
+const cors = require('cors')
+const app = express()
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
